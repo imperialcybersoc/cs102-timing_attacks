9b76a4b09a70d00e1eb27a206d4c9b2684024d75
lemon-gith <77743782+lemon-gith@users.noreply.github.com>
2024-10-10 11:27:09 +0100

    upgrade passwords

--- src/passwords.py
+++ src/passwords.py
@@ -3,7 +3,13 @@ from dataclasses import dataclass, field
 @dataclass(frozen=True, slots=True)
 class PassObject:
 
-    passwords: list[str] = field(default_factory=lambda: ["password", "password123", "password1234", "password12345", "password123456"])
+    passwords: list[str] = field(default_factory=lambda: [
+        "password123",
+        "correct-horse-battery-staple",
+        "myV3rYv£Ry$3cUreP@55w0rd",
+        "=yEH(4z*Y20_8}DJvxN",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris volutpat accumsan dui, id interdum purus finibus ac. Mauris scelerisque efficitur nulla, sit amet imperdiet erat gravida sit amet. Donec aliquam mauris tristique augue pulvinar, non auctor ipsum laoreet. Maecenas vehicula diam id augue mollis interdum. Ut suscipit lectus sit amet lectus hendrerit interdum. Maecenas in purus vel orci pulvinar venenatis in eget ipsum. Aenean finibus purus nec turpis posuere, a pretium lectus bibendum. Aliquam erat volutpat. Proin tincidunt mauris aliquam ex ullamcorper, non pretium libero semper."
+    ])
 
     def get_password(self, index: int) -> str:
         return self.passwords[index]
